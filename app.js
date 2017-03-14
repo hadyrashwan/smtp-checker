275950188473ad9c4639d8cefc97eaaf7d9efa1a
hadyRashwan <h2rashwan@gmail.com>
2017-03-14 11:12:52 +0200

    remove object option

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 var nodemailer = require("nodemailer");
 
-var smtpTransport = nodemailer.createTransport({
-  //  service: "Gmail",  // sets automatically host, port and connection security settings
-   auth: {
-       user: "email@gmail.com",
-       pass: "gmailPassword"
-   }
-});
-// var smtpTransport = nodemailer.createTransport("smtps://mail%40assesstm.com:pass@smtp.gmail.com");
+
+// var smtpTransport = nodemailer.createTransport("smtps://mail%40assesstm.com:pass@smtp.gmail.com"); // PUT YOUR SMTP HERE
 
 smtpTransport.sendMail({  //email options
-   from: "Sender Name <h.rashwan@assesstm.com>", // sender address.  Must be the same as authenticated user if using Gmail.
-   to: "h2rashwan@gmail.com", // receiver
+   from: "Sender Name <a.b@assesstm.com>", // sender address.  Must be the same as authenticated user if using Gmail.
+   to: "a.b@gmail.com", // receiver
    subject: "Emailing with nodemailer", // subject
    text: "Email Example with nodemailer" // body
 }, function(error, response){  //callback
